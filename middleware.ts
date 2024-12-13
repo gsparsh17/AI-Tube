adbfb6c7d7e022d77bd4f89253c6eeb91b1d9651
Sparsh <gsparsh17@gmail.com>
2024-12-14 02:09:05 +0530

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = { matcher: ["/dashboard", "/summarize"] };
+
+console.log("Middleware triggered for route:", config.matcher);
